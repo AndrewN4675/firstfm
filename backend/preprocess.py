05d0a2b6a7d32096ac6c0cbb04f3f8063ebc1ee6
Logan Meyers <theterrarian03@gmail.com>
2025-09-25 15:11:27 -0700

    Added backend folder, and preprocess.py file for processing data

---
+++ backend/preprocess.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import os
+import pickle
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
+# stuf here.... :)
